602b8f944b84c18896500f17cfb7f5f1c09f96c6
Manishk93930 <139479598+Manishk93930@users.noreply.github.com>
2024-08-26 23:24:28 +0530

    Updated myport.js

--- Portfolio/myport.js
+++ Portfolio/myport.js
@@ -62,7 +62,7 @@ ScrollReveal().reveal('.home-content p, .about-content',{origin:'right'});
 
 // ===================== typed.js =====================
 const typed = new Typed('#multiple-text', {
-	strings: ['Frontend', 'Software'],
+	strings: ['Full-Stack', 'Software'],
 	typeSpeed: 100,
 	backSpeed: 100,
 	backDelay: 1000,
